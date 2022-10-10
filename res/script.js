f02d4093df1b55cafe0fb5c57913bcc6c9c55b7a
willc <willcrain1@gmail.com>
2022-10-10 02:02:45 -0400

    Added fetch functionality for location and weather API

--- res/script.js
+++ res/script.js
@@ -1,6 +1,34 @@
 var baseGeoURL = "https://api.openweathermap.org/geo/1.0/direct?q=";
+var baseForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=";
 var apiKey = "6992b9ccb68fd1c4ce20682131e41c4c";
+var unitType = "imperial";
 
-function buildGeoUrl() {
+loadWeatherData("corona");
 
+function buildGeoUrl(city, apiKey) {
+    return baseGeoURL + city + "&apikey=" + apiKey;
+}
+
+function buildForecastUrl(lat, lon, unitType, apiKey) {
+    return baseForecastUrl + lat + "&lon=" + lon + "&units=" + unitType + "&apikey=" + apiKey;
+}
+
+function loadWeatherData(city) {
+    fetch(buildGeoUrl(city, apiKey)).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                var lat = data[0].lat;
+                var lon = data[0].lon;
+                
+                fetch(buildForecastUrl(lat, lon, unitType, apiKey)).then(function(response) {
+                    if(response.ok) {
+                        response.json().then(function(data) {
+                            console.log(data);
+                            console.log(moment.unix(data.list[0].dt).format("MMM Do YYYY"));
+                        });
+                    }
+                });
+            });
+        }
+    });
 }
